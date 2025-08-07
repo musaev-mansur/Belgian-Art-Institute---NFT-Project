1b2910141cffe2e2629b48230538edf1d4e994ab
Mansur-09595 <mansur_musaev_1997@mail.ru>
2025-08-07 18:59:15 +0200

    Fix package.json structure - move tailwindcss to dependencies for Render build v-2

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, '.'),
+      '@/components': require('path').resolve(__dirname, 'components'),
+    }
+    return config
+  },
+}
+
+module.exports = nextConfig
